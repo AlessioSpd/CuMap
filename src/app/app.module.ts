a51da374ea5c6230b120ec657bda88119baaff70
Alessio <93293488+AlessioSpd@users.noreply.github.com>
2023-07-03 17:54:29 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapCommonComponent } from './components/map-common/map-common.component';
+import { HomePageComponent } from './components/home-page/home-page.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapCommonComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/map-common/map-common.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+@Component({
+  selector: 'app-map-common',
+  templateUrl: './map-common.component.html',
+  styleUrls: ['./map-common.component.scss']
+})
+export class MapCommonComponent implements OnInit{
+
+  existingPoint: mapboxgl.Marker[] = [];
+  count: number = 0;
+  
+  ngOnInit(): void {
+
+    (mapboxgl as typeof mapboxgl).accessToken = 'pk.eyJ1IjoicGFtNGs0IiwiYSI6ImNsamxzbmpkNTB6Y2szZXBwdWh1dngwZ3QifQ.kR2_3RYgq9JBYLoHQ7GkeA';
+    
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+      style: 'mapbox://styles/pam4k4/cljlsq5od009201o4ene7fhgp', // style URL
+      center: [12.4963655, 41.9027835], // starting position [lng, lat]
+      zoom: 10 // starting zoom
+    });
+
+    map.on('click', (e) => {
+      let long = e.lngLat.wrap()['lng']
+      let lat = e.lngLat.wrap()['lat']
+      
+      console.log(long + " - " + lat)
+  
+      this.existingPoint.push(new mapboxgl.Marker()
+          .setLngLat([long , lat])
+          .addTo(map)
+      )
+  
+      console.log(this.existingPoint)
+      this.count = this.count + 1;
+      console.log(this.count);
+      if(this.count == 4) {
+        this.existingPoint.map(el=>{el.remove()});
+        this.count = 0;
+      }
+    });
+
+  }
+
+}
